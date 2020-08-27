34a94f77d23cbea5243d3a54f46fc166ae052968
valeriya-tikhonova <v.r.tikhonova@gmail.com>
2020-08-27 18:23:50 +0300

    27.08.2020

--- bandle.js
+++
@@ -1,7 +0,0 @@
-var b=1;
-function sumfum (a, c) {
-    b = a+c;
-}
-function cnslB (){
-    console.log(b)
-}
--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,28 @@
-const intOwl = function(){
-    $(".owl-carousel").owlCarousel(
-        {nav:true}
-    );
-}
-$(document).ready(intOwl)
+// const form = document.getElementById('form');
+// form.onsubmit = function(event) {
+//     //остановить стандартную обработку браузером
+//     event.prevenDefault();
+    
+//     Object.keys(form.elements).forEach(function(elements){
+//         if(form.elements[elements].type != "submit") {
+//             console.log(form.elements[elements].name, form.elements[elements].value)
+//         }
 
-const films= [
+//     })
+// }
 
-];
+const button = $('#submit');
+const form = $('#form');
+button.on('click', function(event){
+    event.preventDefault();
+    let data = form.serialize();
+    console.log(data)
+})
 
+// function ifExist (value){
+//     let error = false;
 
-// console.log(films)
-
-document.getElementById
+//     if (value.trim() == "")
+//         error - true;
+//     return error;
+// }
